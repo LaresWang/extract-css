bfd5ff170642f780c3c7e21f2587de9da2ae3fb4
LaresWang <wang.liang.loveyou@163.com>
2022-01-17 17:58:31 +0800

    umd

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './index.js',
   output: {
+    libraryTarget: 'umd',
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
