b3f0540c24fb2a64720c8623a7351ab6c60dc122
LaresWang <wang.liang.loveyou@163.com>
2022-01-20 10:01:43 +0800

    打包测试

--- core/cssSort.js
+++ core/cssSort.js
@@ -1,6 +1,6 @@
 const utils = require('./utils');
-const { SORTED_PROPERTY } = require('./const');
-const LEN = SORTED_PROPERTY.length;
+const con = require('./const');
+const LEN = con.SORTED_PROPERTY.length;
 
 const sort = function(css){
   if(!utils.isObject(css)){
@@ -9,7 +9,7 @@ const sort = function(css){
   let sorted = [];
   let keys = Object.keys(css);
   for(let i=0; i<LEN; i++){
-    const prop = SORTED_PROPERTY[i];
+    const prop = con.SORTED_PROPERTY[i];
     const index = keys.indexOf(prop);
     if(index>-1){
       sorted.push(prop);
